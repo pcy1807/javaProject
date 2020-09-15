119de4577ea58d0a8d99201b6c2e3fa4e07da32b
pcy1807 <pcy1807@sina.com>
2020-09-15 18:08:20 +0800

    新增2.教师选项

--- src/com/pf/test/demo.java
+++ src/com/pf/test/demo.java
@@ -8,6 +8,7 @@ public class demo {
         System.out.println("请输入你的序号，判断相关职业");
         System.out.println("0.会计");
         System.out.println("1.程序员");
+        System.out.println("2.教师");
         int type = scan.nextInt();
         String profession = (type==0?"会计":"程序员");
         System.out.println(profession);
