872c197981060bdc45c920d9f22b1c8bd0097f01
pcy1807 <pcy1807@sina.com>
2020-09-15 17:56:35 +0800

    pf初始代码提交

---
+++ src/com/pf/test/demo.java
@@ -0,0 +1,18 @@
+package com.pf.test;
+
+import java.util.Scanner;
+
+public class demo {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("请输入你的序号，判断相关职业");
+        System.out.println("0.会计");
+        System.out.println("1.程序员");
+        int type = scan.nextInt();
+        String profession = (type==0?"会计":"程序员");
+        System.out.println(profession);
+    }
+}
+
+
+
